54ce2b676ed5f3cd82dfa3d1f04d7837f88cb950
Rahul Mishra <rahul.mishra2690@gmail.com>
2019-02-23 14:21:33 +0530

    Pic Scraping from a wikipedia page'

---
+++ pic_scraper.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import urllib.request
+
+# Requesting the HTML code of the URL.
+html = requests.request('get', 'https://en.wikipedia.org/wiki/Ramana_Maharshi')
+html.encoding = 'ISO-8859-1' # It makes the HTML source code print nicely
+
+# Providing the HTML sourcecode to bs4 method
+soup = BeautifulSoup(html.text, 'html.parser')
+
+# Searching for all images on the page.
+imageList = []
+contents = soup.find_all('img')
+for i in contents:
+	try:
+		imageList.append(i['src'])
+		# print(i['src'])
+	except:
+		pass
+
+# Saving the image
+for num, i in enumerate(contents):
+	try:
+		urllib.request.urlretrieve('http://' + str(i['src'])[2:], str(num) + '.jpg')
+	except:
+		pass
